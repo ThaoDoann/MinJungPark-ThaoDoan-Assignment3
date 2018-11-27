e1e17352834885632bbec23ffc770eebf6775aa8
Thao Doan <43444987+ThaoDoann@users.noreply.github.com>
2018-11-26 20:35:24 -0500

    Update Controller.java

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -93,7 +93,7 @@ public class Controller extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		doGet(request, response );
 	}
 
 }
